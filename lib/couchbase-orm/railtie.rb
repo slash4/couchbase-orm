59a6157e16f5519d6041a5cb609f0ee139480701
CENTAR Alex <acentar@cosmocoders.fr>
2017-07-21 15:49:16 +0200

    Update railtie.rb

--- lib/couchbase-orm/railtie.rb
+++ lib/couchbase-orm/railtie.rb
@@ -42,11 +42,11 @@ module Rails #:nodoc:
                 }
             end
             
-            begin
-                config.send(:app_generators).orm :couchbase_orm, :migration => false
-            rescue
-                puts "couchbase_orm not fully loaded, config aborted"
-            end
+            #begin
+            #    config.send(:app_generators).orm :couchbase_orm, :migration => false
+            #rescue
+            #    puts "couchbase_orm not fully loaded, config aborted"
+            #end
 
             if config.action_dispatch.rescue_responses
                 config.action_dispatch.rescue_responses.merge!(rescue_responses)
